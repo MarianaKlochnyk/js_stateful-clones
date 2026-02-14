ce590488e3f3813678b697f3d53068452917619a
Mariana Klochnyk <klochnykmaryana@gmail.com>
2026-02-14 16:29:32 +0200

    done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,36 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const history = [];
+  let currentState = { ...state };
+
+  for (const action of actions) {
+    let newState;
+
+    if (action.type === 'clear') {
+      newState = {};
+    }
+
+    if (action.type === 'addProperties') {
+      newState = {
+        ...currentState,
+        ...action.extraData,
+      };
+    }
+
+    if (action.type === 'removeProperties') {
+      newState = { ...currentState };
+
+      for (const key of action.keysToRemove) {
+        delete newState[key];
+      }
+    }
+
+    history.push(newState);
+    currentState = newState;
+  }
+
+  return history;
 }
 
 module.exports = transformStateWithClones;
